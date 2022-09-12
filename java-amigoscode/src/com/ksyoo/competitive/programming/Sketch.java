79e022482a0cbf1d2a53217bc2e0ccdd5212d2ff
OptimistLabyrinth <keesung0705@gmail.com>
2022-09-12 21:24:57 +0900

    create package to write codes when do competitive programming
    readily auto import packages

---
+++ java-amigoscode/src/com/ksyoo/competitive/programming/Sketch.java
@@ -0,0 +1,15 @@
+package com.ksyoo.competitive.programming;
+
+public class Sketch {
+    public static void main(String[] args) {
+        final var result = Solution.solution();
+        System.out.println("result: " + result);
+    }
+
+    static class Solution {
+        public static String solution() {
+            String answer = "hello, world!";
+            return answer;
+        }
+    }
+}
